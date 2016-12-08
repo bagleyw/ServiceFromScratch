39964d16e1c10792ae6fdc37ed59da7f3260c487
BagleyW <BagleyW@HQD2329044L>
2016-12-08 15:55:31 -0800

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EndpointAddress ep = new EndpointAddress("http://Localhost:8000/HelloIndigo/HelloIndigoService");
+
+            IHelloIndigoService proxy = ChannelFactory<IHelloIndigoService>.CreateChannel(new BasicHttpBinding(), ep);
+
+            string s = proxy.HelloIndigo();
+            Console.WriteLine(s);
+
+            Console.WriteLine("Press <ENTER> to terminate client.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloIndigo/Service.cs
@@ -0,0 +1,19 @@
+﻿using System.ServiceModel;
+
+namespace HelloIndigo
+{
+    [ServiceContract(Namespace = "http://www.thatindigogirl.com/samples/2006/06")]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+
+    public class HelloIndigoService : IHelloIndigoService
+    {
+        public string HelloIndigo()
+        {
+            return "Hello Indigo";
+        }
+    }
+}
